1fcbb1583b560fde9a2af6ddde458448e3e35a2e
Sallis-GH <74314359+Sallis-GH@users.noreply.github.com>
2023-01-05 20:39:37 +0100

    delete request finished

--- addons-ts-lab-puppiesApi-main/app.ts
+++ addons-ts-lab-puppiesApi-main/app.ts
@@ -5,9 +5,6 @@ import bodyParser from 'body-parser';
 
 const app: Application = express();
 
-// app.get('/api/test', (_req: Request, res: Response) => {
-//   return res.status(200).json({ test: 'is working as it should' });
-// });
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -24,21 +21,18 @@ app.route('/api/puppies')
     res.status(500).send('Bad Request');
   })
 
-app.get('/api/puppies/:id', (req: Request, res: Response) => {
-  return res.status(200).json(data.filter(puppy => "" + puppy.id === req.params.id));
-});
-
-/* 
-
-Your task is to create a RESTful API with the following endpoints:
-
-- GET: `api/puppies`. This should return a list of all puppies in JSON-format.
-- GET: `api/puppies/:id`. This should return one puppy in JSON-format.
-- POST: `api/puppies`. This should create and return the newly added puppy.
-- PUT: `api/puppies/:id`. This should put one puppy down (jk, just update the specific puppy).
-- DELETE: `api/puppies/:id`. This should actually put one puppy down aka delete it.
-
-*/
-
+app.route('/api/puppies/:id')
+  .get((req: Request, res: Response) => {
+    return res.status(200).json(data.filter(puppy => "" + puppy.id === req.params.id));
+  })
+  .delete((req: Request, res: Response) => {
+    const id = Number(req.params.id)
+    const index = data.findIndex(puppy => puppy.id === id)
+    if (index !== -1) {
+      data.splice(index, 1)
+      res.status(204).send()
+    }
+    res.status(404).send()
+  })
 
 export default app;
