b64634da513d4de9eea270ae5fc909627a083cd1
Sallis-GH <74314359+Sallis-GH@users.noreply.github.com>
2023-01-05 21:13:29 +0100

    added put request, lab completed

--- addons-ts-lab-puppiesApi-main/app.ts
+++ addons-ts-lab-puppiesApi-main/app.ts
@@ -25,14 +25,25 @@ app.route('/api/puppies/:id')
   .get((req: Request, res: Response) => {
     return res.status(200).json(data.filter(puppy => "" + puppy.id === req.params.id));
   })
+  .put((req: Request, res: Response) => {
+    const id = Number(req.params.id)
+    const index = data.findIndex(puppy => puppy.id === id)
+    const { name, breed, birthDate } = req.body
+
+    if (name && breed && birthDate && index !== -1) {
+      data[index] = { id: id, name, breed, birthDate };
+      res.status(200).send("Puppy updated :)")
+    }
+    res.status(404).send();
+  })
   .delete((req: Request, res: Response) => {
     const id = Number(req.params.id)
     const index = data.findIndex(puppy => puppy.id === id)
-    if (index !== -1) {
-      data.splice(index, 1)
-      res.status(204).send()
+    if (index === -1) {
+      res.status(404).send()
     }
-    res.status(404).send()
+    data.splice(index, 1)
+    res.status(204).send()
   })
 
 export default app;
